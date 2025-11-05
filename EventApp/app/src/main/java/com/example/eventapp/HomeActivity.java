9eb03606518153e85e547ea3cc4a0fef9a2c928b
aymonewilson-bahun <aymonew1@gmail.com>
2025-11-05 00:37:10 -0700

    Implement Getting Started button navigation + updated manifest and layout

--- EventApp/app/src/main/java/com/example/eventapp/HomeActivity.java
+++ EventApp/app/src/main/java/com/example/eventapp/HomeActivity.java
@@ -1,56 +1,43 @@
 package com.example.eventapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-
-import androidx.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Make sure the layout name matches your file name exactly
         setContentView(R.layout.home_page);
 
-        // Find the toolbar defined in your XML
+        // Import the toolbar from XML
         Toolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
 
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle("Tappit");
-        }
+        // Buttons from XML
+        Button btnGettingStarted = findViewById(R.id.btnGettingStarted);
+        Button btnViewEvents = findViewById(R.id.button_view_events);
+
+        // "Getting Started" goes to LoginActivity
+        btnGettingStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // "View Events" is placeholder for now
+        btnViewEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Later navigate to BrowseEventsActivity
+            }
+        });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_profile) {
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Profile clicked", Snackbar.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.action_settings) {
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Settings clicked", Snackbar.LENGTH_SHORT).show();
-            return true;
-        } else if (id == android.R.id.home) {
-            Snackbar.make(findViewById(android.R.id.content),
-                    "Home clicked", Snackbar.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
 }
-}
-
