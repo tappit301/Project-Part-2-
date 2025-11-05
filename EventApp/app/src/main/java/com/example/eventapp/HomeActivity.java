e83a3dfea65d5db825ad01a49adbac017ec987ba
aymonewilson-bahun <aymonew1@gmail.com>
2025-11-05 00:59:47 -0700

    Restore homepage layout with Tappit logo, add Profile/Sign In toolbar menu, and implement Getting Started navigation

--- EventApp/app/src/main/java/com/example/eventapp/HomeActivity.java
+++ EventApp/app/src/main/java/com/example/eventapp/HomeActivity.java
@@ -2,42 +2,59 @@ package com.example.eventapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Make sure the layout name matches your file name exactly
         setContentView(R.layout.home_page);
 
-        // Import the toolbar from XML
+        // Toolbar setup
         Toolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
 
-        // Buttons from XML
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Tappit");
+        }
+
+        // Button: Go to LoginActivity when "Getting Started" is clicked
         Button btnGettingStarted = findViewById(R.id.btnGettingStarted);
-        Button btnViewEvents = findViewById(R.id.button_view_events);
-
-        // "Getting Started" goes to LoginActivity
-        btnGettingStarted.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
+        btnGettingStarted.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
+    }
 
-        // "View Events" is placeholder for now
-        btnViewEvents.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: Later navigate to BrowseEventsActivity
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_profile) {
+            Snackbar.make(findViewById(android.R.id.content),
+                    "Profile clicked", Snackbar.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_sign_in) {
+            Snackbar.make(findViewById(android.R.id.content),
+                    "Sign In clicked", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
